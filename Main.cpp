17f917b3d9891c917021968cbf7ba7ed0fc210a6
tbvjchvkfl <anfmdehdnjs@gmail.com>
2023-06-27 23:44:26 +0900

    초기 커밋.

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+void P_Function();
+void P_Function_1();
+
+
+int main()
+{
+	cout << P_Function << endl;
+}
+
+void P_Function()
+{
+
+}
+
+void P_Function_1()
+{
+}
